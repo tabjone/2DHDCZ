4f2a2e854edbe24e0aaeeb6fcbae183b718d82af
Tor-Andreas Bjone <tabjone@Tor-Andreass-MacBook-Air.local>
2023-07-10 21:32:56 +0200

    Testing

--- src/main.c
+++ src/main.c
@@ -5,12 +5,17 @@
 
 #include "functions.h"
 
-
 int main(int argc, char *argv[])
-{
-    double **radius;
-    allocate_2D_array(&radius, 10, 10);
-    deallocate_2D_array(radius);
+{   
+    double ***threeD_test;
+    allocate_3D_array(&threeD_test, 10, 10, 10);
+    deallocate_3D_array(threeD_test);
 
-    
+    double **twoD_test;
+    allocate_2D_array(&twoD_test, 10, 10);
+    deallocate_2D_array(twoD_test);
+
+    double *oneD_test;
+    allocate_1D_array(&oneD_test, 10);
+    deallocate_1D_array(oneD_test);
 }
