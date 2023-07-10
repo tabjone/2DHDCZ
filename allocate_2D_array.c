17bf83a4907d3600253f176a05a114ef78820c09
Tor-Andreas Bjone <tabjone@Tor-Andreass-MacBook-Air.local>
2023-07-10 20:18:10 +0200

    COMMENT: Parameter description

--- allocate_2D_array.c
+++ allocate_2D_array.c
@@ -1,6 +1,18 @@
 #include <stdlib.h>
 
 void allocate_2D_array(double ***array_ptr, int ny, int nx) {
+    /*
+    Allocates memory for a 2D contiguous array.
+
+    Parameters
+    ----------
+    array_ptr : double***
+        Pointer to the array to allocate.
+    ny : int
+        Number of rows in the array.
+    nx : int
+        Number of columns in the array.
+    */
     // Allocate the primary array.
     *array_ptr = malloc(ny * sizeof(**array_ptr));
     if (*array_ptr == NULL) {
