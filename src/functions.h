e2fa710ff12fa31d4da769c0fd7293aa4704f8ad
Tor-Andreas Bjone <tabjone@Tor-Andreass-MacBook-Air.local>
2023-07-11 17:04:43 +0200

    ADD: include for derivatives

--- src/functions.h
+++ src/functions.h
@@ -3,7 +3,10 @@
 
 #include "array_allocation/allocate_functions.h"
 #include "array_deallocation/deallocate_functions.h"
+
 #include "snapshot_saving/snapshot_saving_functions.h"
 #include "snapshot_loading/snapshot_loading_functions.h"
+
+#include "derivatives/derivatives.h"
 
 #endif // functions_H__
