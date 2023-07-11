518f19822fc73852e0013f2340405e8518f671c6
Tor-Andreas Bjone <tabjone@Tor-Andreass-MacBook-Air.local>
2023-07-11 17:01:39 +0200

    ADD: Header for derivatives

---
+++ src/derivatives/derivatives.h
@@ -0,0 +1,7 @@
+#ifndef DERIVATIVES_DERIVATIVES_H_
+#define DERIVATIVES_DERIVATIVES_H_
+
+#include "./time_derivatives/time_derivative_functions.h"
+#include "./spatial_derivatives/spatial_derivative_functions.h"
+
+#endif // DERIVATIVES_DERIVATIVES_H_
