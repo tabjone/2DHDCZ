dd6153db3b67e2cbfc239819bf4e095559b7d235
Tor-Andreas Bjone <tabjone@Tor-Andreass-MacBook-Air.local>
2023-07-21 14:45:20 +0200

    TEST: Interpolation of Solar S data, appears to be working for all r/R values between 0 and 1

--- src/main.c
+++ src/main.c
@@ -5,11 +5,70 @@
 #include "functions.h"
 
 int main(int argc, char *argv[])
-{   
+{
+    int size = 10;
+    double *r_over_R, *c_s, *rho, *p, *Gamma_1, *T;
+    allocate_1D_array(&r_over_R, size);
+    allocate_1D_array(&c_s, size);
+    allocate_1D_array(&rho, size);
+    allocate_1D_array(&p, size);
+    allocate_1D_array(&Gamma_1, size);
+    allocate_1D_array(&T, size);
+
+    for (int i=0; i<size; i++)
+    {
+        r_over_R[i] = 0.1*i/(size-1);
+    }
+
+    read_and_interpolate_solar_s_data(r_over_R, c_s, rho, p, Gamma_1, T, size);
+    printf("Read and interpolated solar_s data\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("r_over_R=%f, c_s=%f, rho=%f, p=%f, Gamma_1=%f, T=%f\n", r_over_R[i], c_s[i], rho[i], p[i], Gamma_1[i], T[i]);
+    }
+
+    deallocate_1D_array(r_over_R);
+    deallocate_1D_array(c_s);
+    deallocate_1D_array(rho);
+    deallocate_1D_array(p);
+    deallocate_1D_array(Gamma_1);
+    deallocate_1D_array(T);
+
+
+    /*
+    int solar_s_data_size = 2482;
+    double *T, *rho, *p, *c_s, *r_over_R, *Gamma_1;
+    allocate_1D_array(&T, solar_s_data_size);
+    allocate_1D_array(&rho, solar_s_data_size);
+    allocate_1D_array(&p, solar_s_data_size);
+    allocate_1D_array(&c_s, solar_s_data_size);
+    allocate_1D_array(&r_over_R, solar_s_data_size);
+    allocate_1D_array(&Gamma_1, solar_s_data_size);
+
+    //read_solar_s_data("../additional_files/solar_s.h5", r_over_R, c_s, rho, p, Gamma_1, T);
+    
+    const char *filename = "../additional_files/solar_s.h5";
+    read_solar_s_data(filename, r_over_R, c_s, rho, p, Gamma_1, T, solar_s_data_size);
+
+    for (int i=0; i < 10; i++)
+    {
+        printf("%f %f %.10f %f %f %f\n", r_over_R[i], c_s[i], rho[i], p[i], Gamma_1[i], T[i]);
+    }
+
+    deallocate_1D_array(T);
+    deallocate_1D_array(rho);
+    deallocate_1D_array(p);
+    deallocate_1D_array(c_s);
+    deallocate_1D_array(r_over_R);
+    deallocate_1D_array(Gamma_1);
+
+    */
     /*
     2D is in the z-y plane
     1D is in the z direction
     */
+   /*
+    printf("%f\n", central_first_derivative_second_order(1,2,1));
 
     double dx, dy, dz;
 
@@ -24,7 +83,6 @@ int main(int argc, char *argv[])
     dx = 1.0/(dims[2]-1);
     dy = 1.0/(dims[1]-1);
     dz = 1.0/(dims[0]-1);
-
 
     double ***threeD_test;
     allocate_3D_array(&threeD_test, dims[0], dims[1], dims[2]);
@@ -55,5 +113,5 @@ int main(int argc, char *argv[])
     
     deallocate_3D_array(threeD_test);
     deallocate_2D_array(twoD_test);
-    deallocate_1D_array(oneD_test);
+    deallocate_1D_array(oneD_test);*/
 }
