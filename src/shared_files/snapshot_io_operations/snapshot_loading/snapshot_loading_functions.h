6f3276f19a420cb00b234a90c43c8bf545f18581
Tor-Andreas Bjone <tabjone@Tor-Andreass-MacBook-Air.local>
2023-07-21 12:43:01 +0200

    UPDATE: Remove solar_s from snapshot_loading_functions.h

--- src/shared_files/snapshot_io_operations/snapshot_loading/snapshot_loading_functions.h
+++ src/shared_files/snapshot_io_operations/snapshot_loading/snapshot_loading_functions.h
@@ -5,6 +5,4 @@ void read_dataset_3D(hid_t file_id, const char* datasetname, hsize_t* dims, doub
 void read_dataset_2D(hid_t file_id, const char* datasetname, hsize_t* dims, double** data);
 void read_dataset_1D(hid_t file_id, const char* datasetname, hsize_t* dims, double* data);
 
-void read_solar_s_data(const char* filename, double* r_over_R, double* c_s, double* rho, double* p, double* Gamma_1, double* T, hsize_t size);
-
 #endif // SNAPSHOT_LOADING_SNAPSHOT_LOADING_FUNCTIONS_H
