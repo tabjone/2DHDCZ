9b102f89dd5ef0fe8dacbd47e1fa8ee978e4596d
Tor-Andreas Bjone <tabjone@Tor-Andreass-MacBook-Air.local>
2023-08-18 02:39:29 +0200

    ADD: Header file for main_hd_2D.c

---
+++ src/hd_solver/2D/main_hd_2D.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_HD_2D_H
+#define MAIN_HD_2D_H
+
+int main_hd_2D(int argc, char *argv[]);
+
+#endif // MAIN_HD_2D_H
--- src/hd_solver/hd_functions.h
+++ src/hd_solver/hd_functions.h
@@ -1,6 +1,6 @@
 #ifndef hd_solver_hd_functions_H__
 #define hd_solver_hd_functions_H__
 
-#include "./2D/hd_2D_functions.h"
+#include "./2D/main_hd_2D.h"
 
 #endif // hd_solver_hd_functions_H__
