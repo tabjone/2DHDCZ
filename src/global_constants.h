e7d9d4e4502650597b2323f55b506b8f3840f792
Tor-Andreas Bjone <tabjone@Tor-Andreass-MacBook-Air.local>
2023-08-26 22:58:35 +0200

    ADD: Header for global constants and parameters

---
+++ src/global_constants.h
@@ -0,0 +1,11 @@
+#ifndef GLOBAL_CONSTANTS_H__
+#define GLOBAL_CONSTANTS_H__
+
+#define MU 0.6 // mean molecular weight
+#define M_U 1.66053907e-24 // atomic mass constant [g]
+#define K_B 1.3807e-16 // boltzmann constant [cm2 g s-2 K-1] 
+#define G 6.6743e-8 // gravitational constant [cm3 g-1 s-2]
+#define R_SUN 6.957e10 // solar radius [cm]
+#define M_SUN 1.989e33 // solar mass [g]
+
+#endif // GLOBAL_CONSTANTS_H__
---
+++ src/global_parameters.h
@@ -0,0 +1,15 @@
+#ifndef GLOBAL_PARAMETERS_H__
+#define GLOBAL_PARAMETERS_H__
+
+#define DIMENSIONS 2
+#define UPWIND_ORDER 2
+#define CENTRAL_ORDER 2
+
+#define R_START 0.6 // In units of solar radii
+#define R_END 0.97 // In units of solar radii
+#define DEL_AD 0.4 // Adiabatic temperature gradient
+
+#define K 0.001
+#define NABLA_AD 0.4
+
+#endif
