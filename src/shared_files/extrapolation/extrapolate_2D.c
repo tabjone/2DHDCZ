17676507059624c27754bda262983500168a9a19
Tor-Andreas Bjone <tabjone@Tor-Andreass-MacBook-Air.local>
2023-09-06 17:39:43 +0200

    ADD: Function for extrapolating 2D grid in z-direction

---
+++ src/shared_files/extrapolation/extrapolate_2D.c
@@ -0,0 +1,31 @@
+#include "../structs/structs.h"
+
+void extrapolate_2D(struct ForegroundVariables2D *foreground_variables)
+{
+    int nz_full = foreground_variables->nz_full;
+    int nz_ghost = foreground_variables->nz_ghost;
+    int nx = foreground_variables->nx;
+
+    // Constant extrapolation
+    #if EXTAPOLATE_GHOST_CELLS == 0
+    for (int i = 0; i < nz_ghost; i++)
+    {
+        for (int j = 0; j < nx; j++)
+        {
+            foreground_variables->p1[i][j] = foreground_variables->p1[nz_ghost][j];
+            foreground_variables->rho1[i][j] = foreground_variables->rho1[nz_ghost][j];
+            foreground_variables->T1[i][j] = foreground_variables->T1[nz_ghost][j];
+            foreground_variables->s1[i][j] = foreground_variables->s1[nz_ghost][j];
+            foreground_variables->vx[i][j] = foreground_variables->vx[nz_ghost][j];
+            foreground_variables->vz[i][j] = foreground_variables->vz[nz_ghost][j];
+            
+            foreground_variables->p1[nz_full - nz_ghost + i][j] = foreground_variables->p1[nz_full - nz_ghost - 1][j];
+            foreground_variables->rho1[nz_full - nz_ghost + i][j] = foreground_variables->rho1[nz_full - nz_ghost - 1][j];
+            foreground_variables->T1[nz_full - nz_ghost + i][j] = foreground_variables->T1[nz_full - nz_ghost - 1][j];
+            foreground_variables->s1[nz_full - nz_ghost + i][j] = foreground_variables->s1[nz_full - nz_ghost - 1][j];
+            foreground_variables->vx[nz_full - nz_ghost + i][j] = foreground_variables->vx[nz_full - nz_ghost - 1][j];
+            foreground_variables->vz[nz_full - nz_ghost + i][j] = foreground_variables->vz[nz_full - nz_ghost - 1][j];
+        }
+    }
+    #endif
+}
--- src/shared_files/extrapolation/extrapolation.h
+++ src/shared_files/extrapolation/extrapolation.h
@@ -1,4 +1,6 @@
 #ifndef EXTRAPOLATION_H
 #define EXTRAPOLATION_H
 
+void extrapolate_2D(struct ForegroundVariables2D *foreground_variables);
+
 #endif // EXTRAPOLATION_H
