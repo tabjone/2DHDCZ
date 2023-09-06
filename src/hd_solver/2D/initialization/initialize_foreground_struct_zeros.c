9d042feaf6fda70e550894e39a63df2dbdd27cdf
Tor-Andreas Bjone <tabjone@Tor-Andreass-MacBook-Air.local>
2023-09-06 17:41:41 +0200

    UPDATE: zero-initialization has extrapolation added, but don't really need

--- src/hd_solver/2D/initialization/initialize_foreground_struct_zeros.c
+++ src/hd_solver/2D/initialization/initialize_foreground_struct_zeros.c
@@ -1,14 +1,17 @@
 #include "../../../shared_files/structs/structs.h"
 #include "./initialization.h"
+#include "../../../global_parameters.h"
+#include "../../../shared_files/extrapolation/extrapolation.h"
 
 void initialize_foreground_struct_zeros(struct ForegroundVariables2D *foreground_variables)
 {
     int nz = foreground_variables->nz;
+    int nz_ghost = foreground_variables->nz_ghost;
     int nx = foreground_variables->nx;
 
     int i, j;
 
-    for (i = 0; i < nz; i++)
+    for (i = nz_ghost; i < nz+nz_ghost; i++)
     {
         for (j = 0; j < nx; j++)
         {
@@ -20,4 +23,6 @@ void initialize_foreground_struct_zeros(struct ForegroundVariables2D *foreground
             foreground_variables->vz[i][j] = 0.0;
         }
     }
+
+    extrapolate_2D(foreground_variables);
 }
