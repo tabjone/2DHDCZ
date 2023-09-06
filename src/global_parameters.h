5ce5ecc0cb6a880e64c5ecd5e77a85aebcc41d7b
Tor-Andreas Bjone <tabjone@Tor-Andreass-MacBook-Air.local>
2023-09-06 14:08:58 +0200

    ADD: Directory input in global parameters, create dir in main.c

--- src/global_parameters.h
+++ src/global_parameters.h
@@ -1,6 +1,8 @@
 #ifndef GLOBAL_PARAMETERS_H__
 #define GLOBAL_PARAMETERS_H__
 
+#define RUN_NAME "HD_2D_1"
+
 #define DIMENSIONS 2
 #define MHD 0
 
@@ -12,7 +14,13 @@
 #define DEL_AD 0.4 // Adiabatic temperature gradient
 #define GAMMA 5.0/3 // Superadiabatic parameter
 
-#define K 0.001
+#define K 0.01 // superadiabacicity parameter in CZ
+#define p 0.1 // Number for determining step-size in space
 #define NABLA_AD 0.4
+
+#define x_size 0.01 // In units of solar radii
+#define dx 0.0001 // Step-size in x-direction in units of solar radii
+#define dy 0.0001 // Step-size in y-direction in units of solar radii
+#define dz 0.0001 // Step-size in z-direction in units of solar radii
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdlib.h>
 #include "hdf5.h"
 
@@ -7,6 +10,21 @@
 
 int main(int argc, char *argv[])
 {
+    char dir_name[100];
+    struct stat st = {0}; // Initialize the stat structure
+
+    // Use snprintf to create the directory path with the base directory and RUN_NAME
+    snprintf(dir_name, sizeof(dir_name), "../data/%s", RUN_NAME);
+
+    // Check if directory exists
+    if (stat(dir_name, &st) == -1) {
+        // If directory doesn't exist, create it with permissions set to rwxr-xr-x
+        if (mkdir(dir_name, 0755) == -1) {
+            perror("Error creating directory");
+            return 1;
+        }
+    }
+
     #if MHD == 0
         #if DIMENSIONS == 1
             printf("Not implemented yet\n");
