3b1fea9bb8db34defad77b9286dd13b34fa50026
Tor-Andreas Bjone <tabjone@Tor-Andreass-MacBook-Air.local>
2023-09-06 14:10:06 +0200

    ADD: Interpolation to grid

--- src/shared_files/solar_s_initialization/solar_s_background_initialization.c
+++ src/shared_files/solar_s_initialization/solar_s_background_initialization.c
@@ -3,14 +3,12 @@
 #include "../..//global_parameters.h"
 #include "../../global_constants.h"
 #include "../structs/structs.h"
-#include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include "../interpolation/interpolation.h"
 
 void solar_s_background_initialization(struct BackgroundVariables *bg)
 {
-    float p = 0.1; // number for determining the step size
-
     // Creating arrays for solar_s data
     int solar_s_size = 2482;
     double *r_over_R_solar_s, *rho_solar_s, *p_solar_s, *T_solar_s;
@@ -105,7 +103,7 @@ void solar_s_background_initialization(struct BackgroundVariables *bg)
     {   
         if (r_up[i]>=r_i)
         {
-            k = 0.01;
+            k = K;
         }
         else
         {
@@ -167,7 +165,7 @@ void solar_s_background_initialization(struct BackgroundVariables *bg)
     {   
         if (r_down[j]>=r_i)
         {
-            k = 0.01;
+            k = K;
         }
         else
         {
@@ -222,30 +220,37 @@ void solar_s_background_initialization(struct BackgroundVariables *bg)
     dp_dr = - G * m_down[j] /pow(r_down[j],2) * rho_down[j];
     grad_s0_down[j] = (nabla_star-NABLA_AD) / (rho_down[j] * T_down[j]) * dp_dr;
 
-    //allocating memory for background struct
-    allocate_background_struct(i+j, bg);
-    bg->nz = i+j;
+    int nz_upwdown = i+j;
+
+    // Combining the upward and downward integration arrays
+    double *r = malloc(sizeof(double)*(nz_upwdown));
+    double *p0 = malloc(sizeof(double)*(nz_upwdown));
+    double *T0 = malloc(sizeof(double)*(nz_upwdown));
+    double *rho0 = malloc(sizeof(double)*(nz_upwdown));
+    double *grad_s0 = malloc(sizeof(double)*(nz_upwdown));
+    double *g = malloc(sizeof(double)*(nz_upwdown));
 
     for (int jj = j; jj > 0; jj--)
     {
-        bg->r[j-jj] = r_down[jj];
-        bg->p0[j-jj] = p_down[jj];
-        bg->T0[j-jj] = T_down[jj];
-        bg->grad_s0[j-jj] = grad_s0_down[jj];
-        bg->rho0[j-jj] = rho_down[jj];
-        bg->g[j-jj] = -G * m_down[jj] / pow(r_down[jj],2);
+        r[j-jj] = r_down[jj];
+        p0[j-jj] = p_down[jj];
+        T0[j-jj] = T_down[jj];
+        grad_s0[j-jj] = grad_s0_down[jj];
+        rho0[j-jj] = rho_down[jj];
+        g[j-jj] = -G * m_down[jj] / pow(r_down[jj],2);
     }
 
     for (int ii = j; ii < i+j; ii++)
     {
-        bg->r[ii] = r_up[ii-j];
-        bg->p0[ii] = p_up[ii-j];
-        bg->T0[ii] = T_up[ii-j];
-        bg->grad_s0[ii] = grad_s0_up[ii-j];
-        bg->rho0[ii] = rho_up[ii-j];
-        bg->g[ii] = -G * m_up[ii-j] / pow(r_up[ii-j],2);
+        r[ii] = r_up[ii-j];
+        p0[ii] = p_up[ii-j];
+        T0[ii] = T_up[ii-j];
+        grad_s0[ii] = grad_s0_up[ii-j];
+        rho0[ii] = rho_up[ii-j];
+        g[ii] = -G * m_up[ii-j] / pow(r_up[ii-j],2);
     }
 
+    // Deallocating integration arrays
     free(r_up);
     free(p_up);
     free(m_up);
@@ -261,4 +266,55 @@ void solar_s_background_initialization(struct BackgroundVariables *bg)
     free(s_down);
     free(rho_down);
     free(grad_s0_down);
+
+    // Initialize background radius array
+    for (i = 0; i < bg->nz; i++)
+    {
+        bg->r[i] = R_SUN * R_START + i * R_SUN * (R_END - R_START) / (bg->nz-1.0);
+    }
+
+    // Interpolating the background variables to the grid
+    for (i = 0; i < bg->nz; i++)
+    {
+        // Handle edge cases
+        if (bg->r[i] < r[0])
+        {
+            bg->p0[i] = p0[0];
+            bg->T0[i] = T0[0];
+            bg->rho0[i] = rho0[0];
+            bg->grad_s0[i] = grad_s0[0];
+            bg->g[i] = g[0];
+        }
+        else if (bg->r[i] > r[nz_upwdown-1])
+        {
+            bg->p0[i] = p0[nz_upwdown-1];
+            bg->T0[i] = T0[nz_upwdown-1];
+            bg->rho0[i] = rho0[nz_upwdown-1];
+            bg->grad_s0[i] = grad_s0[nz_upwdown-1];
+            bg->g[i] = g[nz_upwdown-1];
+        }
+        else
+        {
+            int k = 0;
+            while (bg->r[i] > r[k])
+            {
+                k++;
+            }
+            double x0 = r[k-1];
+            double x1 = r[k];
+            bg->p0[i] = interpolate_1D_linear(x0, x1, p0[k-1], p0[k], bg->r[i]);
+            bg->T0[i] = interpolate_1D_linear(x0, x1, T0[k-1], T0[k], bg->r[i]);
+            bg->rho0[i] = interpolate_1D_linear(x0, x1, rho0[k-1], rho0[k], bg->r[i]);
+            bg->grad_s0[i] = interpolate_1D_linear(x0, x1, grad_s0[k-1], grad_s0[k], bg->r[i]);
+            bg->g[i] = interpolate_1D_linear(x0, x1, g[k-1], g[k], bg->r[i]);
+        }
+    }
+
+    // Deallocating integration arrays
+    free(r);
+    free(p0);
+    free(T0);
+    free(rho0);
+    free(grad_s0);
+    free(g);
 }
