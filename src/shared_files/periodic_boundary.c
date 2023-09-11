cde1f13d48db0b5c953b388d2944c0ea99baa1f9
Tor-Andreas Bjone <tabjone@Tor-Andreass-MacBook-Air.local>
2023-09-11 15:27:05 +0200

    MOVE: Period boundary function moved to shared files

---
+++ src/shared_files/periodic_boundary.c
@@ -0,0 +1,24 @@
+int periodic_boundary(int i, int limit) 
+{
+    /*
+    Calculates the new index for a given index under periodic boundary conditions.
+
+    Parameters
+    ----------
+    i : int
+        Index
+    limit : int
+        Size of array
+
+    Returns
+    -------
+    int
+        New index i given periodic boundary conditions
+
+    Ex.
+    ----
+    i = -1, limit = 5 -> 4 (or 5?)
+    i = 5, limit = 5 -> 0
+    */
+    return (i + limit) % (limit);
+}
--- src/shared_files/shared_files.h
+++ src/shared_files/shared_files.h
@@ -1,11 +1,13 @@
 #ifndef SHARED_FILES_SHARED_FILES_H_
 #define SHARED_FILES_SHARED_FILES_H_
 
+int periodic_boundary(int i, int limit);
+
 #include "hdf5.h"
 
 #include "./structs/structs.h"
 #include "./array_memory_management/array_memory_management.h"
-#include "./derivatives/derivatives.h"
+#include "./spacial_derivatives/spacial_derivatives.h"
 #include "./snapshot_io_operations/snapshot_io_operations.h"
 #include "./solar_s_initialization/solar_s_initialization.h"
 #include "./interpolation/interpolation.h"
