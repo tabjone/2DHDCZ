9807f4862412c9d56e0ff6ba3ade77853a1360a7
Tor-Andreas Bjone <tabjone@Tor-Andreass-MacBook-Air.local>
2023-09-21 01:09:36 +0200

    UPDATE: New functions

--- src/hd_solver/2D/functions.h
+++ src/hd_solver/2D/functions.h
@@ -7,16 +7,14 @@
 
 #include "./solve_elliptic_equation/solve_elliptic_equation.h"
 #include "./solve_diff_eqs/solve_diff_eqs.h"
+#include "./one_time_step/one_time_step.h"
 
 #include "./initialization/initialization.h"
 #include "shared_files.h"
 #include "global_parameters.h"
 #include "global_constants.h"
 
-
 #include "io_functions/io_functions.h"
 
-
-void one_time_step(struct BackgroundVariables *bg, struct ForegroundVariables2D *fg_prev, struct ForegroundVariables2D *fg);
 
 #endif // FUNCTIONS_H__
