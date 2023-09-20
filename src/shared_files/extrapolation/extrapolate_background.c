b8cdff76ec2e5dc951ce461de5a7f97716c3d84f
Tor-Andreas Bjone <tabjone@Tor-Andreass-MacBook-Air.local>
2023-09-21 01:03:59 +0200

    NOTE: This has to be changed for MPI implementation

--- src/shared_files/extrapolation/extrapolate_background.c
+++ src/shared_files/extrapolation/extrapolate_background.c
@@ -5,19 +5,20 @@ void extrapolate_background(struct BackgroundVariables *bg)
 {
     int nz_full = bg->nz_full;
     int nz_ghost = bg->nz_ghost;
+    double dz = bg->dz;
 
     // Constant extrapolation
     #if EXTAPOLATE_GHOST_CELLS == 0
     for (int i = 0; i < nz_ghost; i++)
     {
-        bg->r[i] = bg->r[bg->nz_ghost]-(bg->nz_ghost-i)*dz*R_SUN;
+        bg->r[i] = bg->r[bg->nz_ghost]-(bg->nz_ghost-i)*dz;
         bg->p0[i] = bg->p0[nz_ghost];
         bg->rho0[i] = bg->rho0[nz_ghost];
         bg->T0[i] = bg->T0[nz_ghost];
         bg->grad_s0[i] = bg->grad_s0[nz_ghost];
         bg->g[i] = bg->g[nz_ghost];
 
-        bg->r[bg->nz+bg->nz_ghost+i] = bg->r[bg->nz_full-bg->nz_ghost-1]+(i+1)*dz*R_SUN;
+        bg->r[bg->nz+bg->nz_ghost+i] = bg->r[bg->nz_full-bg->nz_ghost-1]+(i+1)*dz;
         bg->p0[nz_full - nz_ghost + i] = bg->p0[nz_full - nz_ghost - 1];
         bg->rho0[nz_full - nz_ghost + i] = bg->rho0[nz_full - nz_ghost - 1];
         bg->T0[nz_full - nz_ghost + i] = bg->T0[nz_full - nz_ghost - 1];
--- src/shared_files/extrapolation/extrapolation.h
+++ src/shared_files/extrapolation/extrapolation.h
@@ -5,5 +5,6 @@
 
 void extrapolate_2D(struct ForegroundVariables2D *fg);
 void extrapolate_background(struct BackgroundVariables *bg);
+void extrapolate_2D_array(double **array, int nz_full, int nz_ghost, int nx);
 
 #endif // EXTRAPOLATION_H
