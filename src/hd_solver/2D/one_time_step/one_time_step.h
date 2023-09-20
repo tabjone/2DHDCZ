8443b3ede2135287b399b41ea3d2f6a679c53092
Tor-Andreas Bjone <tabjone@Tor-Andreass-MacBook-Air.local>
2023-09-21 01:10:56 +0200

    UPDATE: Changed rk1 to fast gauss-seidel, clean up junk in one_time_step.c

--- src/hd_solver/2D/one_time_step/one_time_step.h
+++ src/hd_solver/2D/one_time_step/one_time_step.h
@@ -4,9 +4,9 @@
 #include "../functions.h"
 #include "shared_files.h"
 
-void rk1(struct BackgroundVariables *bg, struct ForegroundVariables2D *fg_prev, struct ForegroundVariables2D *fg);
+void rk1(struct BackgroundVariables *bg, struct ForegroundVariables2D *fg_prev, struct ForegroundVariables2D *fg, double dt);
 
 
-void one_time_step(struct BackgroundVariables *bg, struct ForegroundVariables2D *fg_prev, struct ForegroundVariables2D *fg);
+void one_time_step(struct BackgroundVariables *bg, struct ForegroundVariables2D *fg_prev, struct ForegroundVariables2D *fg, double dt);
 
 #endif // ONE_TIME_STEP_H__
--- src/hd_solver/2D/one_time_step/rk1.c
+++ src/hd_solver/2D/one_time_step/rk1.c
@@ -1,45 +1,90 @@
 #include "one_time_step.h"
 
-void rk1(struct BackgroundVariables *bg, struct ForegroundVariables2D *fg_prev, struct ForegroundVariables2D *fg)
+void rk1(struct BackgroundVariables *bg, struct ForegroundVariables2D *fg_prev, struct ForegroundVariables2D *fg, double dt)
 {
-    double dt = 0.1;
+    // Steps for each iteration:
+    // Solve elliptic equation
+    // Extrapolate p1, v1, s1
+    // Solve diffeqs
+    // Solve algebraic equations
+    // Solve elliptic equation again
 
-    int nx = fg->nx;
     int nz_ghost = fg->nz_ghost;
     int nz_full = fg->nz_full;
-    //int nz = fg->nz;
+    int nx = fg->nx;
+    //double dz = fg_prev->dz;
+    //double dx = fg_prev->dx;
 
+    // Burde jeg ikke egentlig gjøre dette til slutt? Og kanskje før man starter å løse
+    solve_elliptic_equation(bg, fg_prev); // Getting p1
+
+    // Setting updated values of p1 to fg
+    for (int i = 0; i < nz_full; i++)
+    {
+        for (int j = 0; j < nx; j++)
+        {
+            fg->p1[i][j] = fg_prev->p1[i][j];
+        }
+    }
+
+    extrapolate_2D_array(fg->p1, nz_full, nz_ghost, nx);
+
+    // First finding dt
+    for (int i = nz_ghost; i < nz_full - nz_ghost; i++)
+    {
+        for (int j = 0; j < nx; j++)
+        {
+            //dt = C / (fg_prev->vx[i][j]/dx + fg_prev->vz[i][j]/dz);
+        }
+    }
+
+    // Solving diff eqs
     for (int i = nz_ghost+1; i < nz_full - nz_ghost-1; i++)
     {
         for (int j = 0; j < nx; j++)
         {
-            fg->s1[i][j] = fg_prev->s1[i][j] + dt * rhs_ds1_dt(bg, fg_prev, i, j);;
-            fg->vx[i][j] = fg_prev->vx[i][j] + dt * rhs_dvx_dt(bg, fg_prev, i, j);;
-            fg->vz[i][j] = fg_prev->vz[i][j] + dt * rhs_dvz_dt(bg, fg_prev, i, j);;
+            fg->s1[i][j] = fg_prev->s1[i][j] + dt * rhs_ds1_dt(bg, fg_prev, i, j);
+            fg->vx[i][j] = fg_prev->vx[i][j] + dt * rhs_dvx_dt(bg, fg_prev, i, j);
+            fg->vz[i][j] = fg_prev->vz[i][j] + dt * rhs_dvz_dt(bg, fg_prev, i, j);
         }
     }
     
-
-    // OR SHOULD s1 BE ZERO AT THE BOUNDARIES????
-    /*
-    
-    */
+    // Solving boundaries
     for (int j = 0; j < nx; j++)
     {
         // Top boundary
-        fg->s1[nz_ghost][j] = rhs_ds1_dt_vertical_boundary(bg, fg_prev, nz_ghost, j);
+        fg->s1[nz_ghost][j] = 0.0;
         fg->vx[nz_ghost][j] = rhs_dvx_dt_vertical_boundary(bg, fg_prev, nz_ghost, j);
-        fg->vz[nz_full-nz_ghost-1][j] = 0.0;
+        fg->vz[nz_ghost][j] = 0.0;
 
         // Bottom boundary
-        fg->s1[nz_full-nz_ghost-1][j] = rhs_ds1_dt_vertical_boundary(bg, fg_prev, nz_full-nz_ghost-1, j);
+        fg->s1[nz_full-nz_ghost-1][j] = 0.0;
         fg->vx[nz_full-nz_ghost-1][j] = rhs_dvx_dt_vertical_boundary(bg, fg_prev, nz_full-nz_ghost-1, j);
         fg->vz[nz_full-nz_ghost-1][j] = 0.0;
     }
+    
 
-    solve_elliptic_equation(bg, fg); // Getting p1
+    // Extrapolate
+    extrapolate_2D_array(fg->s1, nz_full, nz_ghost, nx);
+    extrapolate_2D_array(fg->vx, nz_full, nz_ghost, nx);
+    extrapolate_2D_array(fg->vz, nz_full, nz_ghost, nx);
 
-    //solve_first_law_of_thermodynamics(bg, fg); // Getting T1
 
-    //solve_equation_of_state(bg, fg); // Getting rho1
+    // Solving algebraic equations. Eq of state should be in separate function
+    double c_p;
+    double total_density, total_temperature, total_pressure;
+    for (int i = 0; i < nz_full; i++)
+    {
+        for (int j = 0; j < nx; j++)
+        {
+            total_density = bg->rho0[i] + fg->rho1[i][j];
+            total_temperature = bg->T0[i] + fg->T1[i][j];
+            total_pressure = bg->p0[i] + fg->p1[i][j];
+            c_p = total_pressure/(1-1/GAMMA) / (total_density*total_temperature);
+
+            fg->T1[i][j] = (fg->s1[i][j]/c_p + (GAMMA-1.0)/GAMMA * fg->p1[i][j]/bg->p0[i]) * bg->T0[i];
+            
+            fg->rho1[i][j] = (fg->p1[i][j]/bg->p0[i] - fg->T1[i][j]/bg->T0[i]) * bg->rho0[i];
+        }
+    }
 }
