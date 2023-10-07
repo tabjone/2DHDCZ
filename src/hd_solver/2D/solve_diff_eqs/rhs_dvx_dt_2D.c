cbbb73a3c7d0d19b034d46211141adcad8c947b8
Tor-Andreas Bjone <tabjone@Tor-Andreass-MacBook-Air.local>
2023-10-07 03:35:05 +0200

    ..

--- src/hd_solver/2D/solve_diff_eqs/rhs_dvx_dt_2D.c
+++
@@ -1,6 +0,0 @@
-#include "solve_diff_eqs.h"
-
-FLOAT_P rhs_dvx_dt_2D(struct BackgroundVariables *bg, struct ForegroundVariables2D *fg, struct GridInfo *grid_info, int i, int j)
-{
-    // REMOVE THIS FUNCTION
-}
