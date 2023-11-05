01088258cb927127ace8e16c35c6d3c7219a2878
Tor-Andreas Stensaker Bjone <tabjone@stjerne2.uio.no>
2023-11-05 10:26:29 +0100

    .

--- src/solver_2D/boundary/update_vertical_boundary_ghostcells_2D.c
+++ src/solver_2D/boundary/update_vertical_boundary_ghostcells_2D.c
@@ -2,23 +2,28 @@
 
 void update_vertical_boundary_ghostcells_2D(FLOAT_P **array, struct GridInfo2D *grid_info, struct MpiInfo *mpi_info)
 {
-    FLOAT_P nz_ghost = grid_info->nz_ghost;
-    FLOAT_P nz_full = grid_info->nz_full;
-    FLOAT_P ny = grid_info->ny;
+    int nz_ghost = grid_info->nz_ghost;
+    int nz_full = grid_info->nz_full;
+    int ny = grid_info->ny;
+
+    int size = mpi_info->size;
 
     #if MPI_ON == 1
         // Sending and receiving ghost cells
         communicate_2D_ghost_above_below(array, grid_info, mpi_info);
     #endif // MPI_ON
 
-    if (!mpi_info->has_neighbor_below)
-    {
-        extrapolate_2D_array_down(array, nz_ghost, ny);
-    }
-    if (!mpi_info->has_neighbor_above)
-    {
-        extrapolate_2D_array_up(array, nz_full, nz_ghost, ny);
-    }
+    #if VERTICAL_BOUNDARY_TYPE != 2
+        // If not periodic boundary extrapolate ghost cells at top and bottom
+        if (!mpi_info->has_neighbor_below)
+        {
+            extrapolate_2D_array_down(array, nz_ghost, ny);
+        }
+        if (!mpi_info->has_neighbor_above)
+        {
+            extrapolate_2D_array_up(array, nz_full, nz_ghost, ny);
+        }
+    #endif // VERTICAL_BOUNDARY_TYPE
 
     // Handle case of periodic boundary
     #if VERTICAL_BOUNDARY_TYPE == 2
@@ -39,6 +44,7 @@ void update_vertical_boundary_ghostcells_2D(FLOAT_P **array, struct GridInfo2D *
                 MPI_Send(&array[nz_ghost][0], nz_ghost*ny, MPI_FLOAT_P, size-1, 1, MPI_COMM_WORLD)
             }
         #else
+            // If not mpi just do periodic boundary function
             periodic_boundary_2D(array, grid_info);
         #endif // MPI_ON
     #endif // VERTICAL_BOUNDARY_TYPE
