6ba4d4fd4b70adbc9d8890ccd1c7ec867448d0bc
Tor-Andreas Stensaker Bjone <tabjone@stjerne2.uio.no>
2023-12-21 22:59:12 +0100

    RM: Old structs

--- src/shared_files/structs/deallocate_grid_info_struct_2D.c
+++
@@ -1,7 +0,0 @@
-#include "structs.h"
-#include <stdlib.h>
-
-void deallocate_grid_info_struct_2D(struct GridInfo2D *grid_info)
-{
-    free(grid_info);
-}
--- src/shared_files/structs/deallocate_grid_info_struct_3D.c
+++
@@ -1,7 +0,0 @@
-#include "structs.h"
-#include <stdlib.h>
-
-void deallocate_grid_info_struct_3D(struct GridInfo3D *grid_info)
-{
-    free(grid_info);
-}
