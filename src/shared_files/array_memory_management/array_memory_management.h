58480465cd27d19fba01c840f52bc24aeaa0a65e
Tor-Andreas Stensaker Bjone <tabjone@stjerne2.uio.no>
2023-12-21 14:59:28 +0100

    RM: Old folders

--- src/shared_files/array_memory_management/array_memory_management.h
+++
@@ -1,7 +0,0 @@
-#ifndef ARRAY_MEMORY_MANAGEMENT_ARRAY_MEMORY_MANAGEMENT_H_
-#define ARRAY_MEMORY_MANAGEMENT_ARRAY_MEMORY_MANAGEMENT_H_
-
-#include "./array_allocation/allocate_functions.h"
-#include "./array_deallocation/deallocate_functions.h"
-
-#endif // ARRAY_MEMORY_MANAGEMENT_ARRAY_MEMORY_MANAGEMENT_H_
--- src/shared_files/interpolation/interpolation.h
+++
@@ -1,8 +0,0 @@
-#ifndef INTERPOLATION_H
-#define INTERPOLATION_H
-
-#include "global_parameters.h"
-
-FLOAT_P interpolate_1D_linear(FLOAT_P x0, FLOAT_P x1, FLOAT_P y0, FLOAT_P y1, FLOAT_P x);
-
-#endif // INTERPOLATION_H
