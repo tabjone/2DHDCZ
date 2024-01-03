d426b0def8817b2dc4d8a983a8483c7b2de1fd96
Tor-Andreas Stensaker Bjone <tabjone@stjerne2.uio.no>
2024-01-03 14:03:47 +0100

    ADD: one_time_step_1D.c function

--- src/solver/one_time_step/one_time_step_1D/one_time_step_1D.c
+++ src/solver/one_time_step/one_time_step_1D/one_time_step_1D.c
@@ -1,7 +1,7 @@
-#include "one_time_step_2D.h"
+#include "one_time_step_1D.h"
 #include "global_parameters.h"
 
-FLOAT_P one_time_step_2D(struct BackgroundVariables *bg, struct ForegroundVariables2D *fg_prev, struct ForegroundVariables2D *fg, struct GridInfo2D *grid_info, struct MpiInfo *mpi_info, struct PrecalculatedVariables2D *precalc, FLOAT_P dt_last, bool first_timestep)
+FLOAT_P one_time_step_1D(struct BackgroundVariables *bg, struct ForegroundVariables1D *fg_prev, struct ForegroundVariables1D *fg, struct GridInfo1D *grid_info, struct MpiInfo *mpi_info, struct PrecalculatedVariables1D *precalc, FLOAT_P dt_last, bool first_timestep)
 {   
     /*
     Calculates the foreground at the next timestep using Runge-Kutta methods.
@@ -11,11 +11,11 @@ FLOAT_P one_time_step_2D(struct BackgroundVariables *bg, struct ForegroundVariab
     bg : struct
         A pointer to the BackgroundVariables struct.
     fg_prev : struct
-        A pointer to the ForegroundVariables2D struct at the previous timestep.
+        A pointer to the ForegroundVariables1D struct at the previous timestep.
     fg : struct
-        A pointer to the ForegroundVariables2D struct at the current timestep.
+        A pointer to the ForegroundVariables1D struct at the current timestep.
     grid_info : struct
-        A pointer to the GridInfo2D struct.
+        A pointer to the GridInfo1D struct.
     mpi_info : struct
         A pointer to the MpiInfo struct.
     dt_last : FLOAT_P
@@ -32,11 +32,11 @@ FLOAT_P one_time_step_2D(struct BackgroundVariables *bg, struct ForegroundVariab
     FLOAT_P dt = 0.0;
 
     #if TIME_ORDER == 1
-        dt = rk1_2D(bg, fg_prev, fg, grid_info, mpi_info, precalc, dt_last, first_timestep);
+        dt = rk1_1D(bg, fg_prev, fg, grid_info, mpi_info, precalc, dt_last, first_timestep);
     #elif TIME_ORDER == 2
-        dt = rk2_2D(bg, fg_prev, fg, grid_info, mpi_info, precalc, dt_last, first_timestep);
+        dt = rk2_1D(bg, fg_prev, fg, grid_info, mpi_info, precalc, dt_last, first_timestep);
     #elif TIME_ORDER == 3
-        dt = rk3_2D(bg, fg_prev, fg, grid_info, mpi_info, precalc, dt_last, first_timestep);
+        dt = rk3_1D(bg, fg_prev, fg, grid_info, mpi_info, precalc, dt_last, first_timestep);
     #endif // TIME_ORDER
 
     return dt;
