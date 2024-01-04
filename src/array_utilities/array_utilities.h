b50e14561d6983598671ccc11cda32d662185c67
Tor-Andreas Stensaker Bjone <tabjone@stjerne2.uio.no>
2024-01-04 20:13:39 +0100

    BUGFIX: Error in header

--- src/array_utilities/array_utilities.h
+++ src/array_utilities/array_utilities.h
@@ -1,7 +1,7 @@
 #ifndef ARRAY_UTILITIES_H_
 #define ARRAY_UTILITIES_H_
 
-#include "./array_memory_management.h"
+#include "./array_memory_management/array_memory_management.h"
 #include "./array_copy/array_copy.h"
 
 #endif // ARRAY_UTILITIES_H_
