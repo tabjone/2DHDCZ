b5495559a04116ee36525f789f90a665912fa48d
Tor-Andreas Stensaker Bjone <tabjone@stjerne2.uio.no>
2024-01-06 15:51:15 +0100

    BUGFIX: upward anti symmetric and symmetric extrapolation was not correct. Also some definition problem in symmetric down 3D

--- src/curve_fitting/extrapolation/extrapolation_2D/extrapolate_2D_array_symmetric_up.c
+++ src/curve_fitting/extrapolation/extrapolation_2D/extrapolate_2D_array_symmetric_up.c
@@ -2,11 +2,15 @@
 
 void extrapolate_2D_array_symmetric_up(FLOAT_P **array, int nz_full, int nz_ghost, int ny) 
 {
+
+    int start_ghost = nz_full - nz_ghost;
+
     for (int i = 0; i < nz_ghost; i++) 
     {
+        int mirror_row = start_ghost - i - 2;
         for (int j = 0; j < ny; j++) 
         {
-            array[nz_full - 1 - i][j] = array[nz_full - nz_ghost - 1 + i][j];
+            array[start_ghost + i][j] = array[mirror_row][j];
         }
     }
 }
--- src/curve_fitting/extrapolation/extrapolation_3D/extrapolate_3D_array_antisymmetric_up.c
+++ src/curve_fitting/extrapolation/extrapolation_3D/extrapolate_3D_array_antisymmetric_up.c
@@ -2,13 +2,16 @@
 
 void extrapolate_3D_array_antisymmetric_up(FLOAT_P ***array, int nz_full, int nz_ghost, int ny, int nx) 
 {
+    int start_ghost = nz_full - nz_ghost;
+
     for (int i = 0; i < nz_ghost; i++) 
     {
+        int mirror_row = start_ghost - i - 2;
         for (int j = 0; j < ny; j++) 
         {
             for (int k = 0; k < nx; k++)
             {
-                array[nz_full - 1 - i][j][k] = -array[nz_full - nz_ghost - 1 + i][j][k];
+                array[start_ghost + i][j][k] = -array[mirror_row][j][k];
             }
         }
     }
